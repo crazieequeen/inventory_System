2f513d159a50e730d5e4a579f82968caf20fd9ac
Kaviya2408 <kit27.csbs29@gmail.com>
2026-03-22 13:59:01 +0530

    fixed mime type issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Inventory_System/', // Matches your GitHub repo name
   plugins: [react()],
   server: {
     port: 3000,
